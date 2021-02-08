849ac8e4e8e92f53980cc2720fc3aa2bcd685ccf
viskos <viskoos.fd@gmail.com>
2021-02-08 19:40:08 +0300

    delete old store

--- src/vanillaStore/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import login from './auth';
-
-export default {
-	...login,
-};
--- src/vanillaStore/sagas/index.js
+++
@@ -1,7 +0,0 @@
-import { all, fork } from 'redux-saga/effects';
-
-import login from './auth';
-
-export default function* root() {
-	yield all([fork(login)]);
-}
