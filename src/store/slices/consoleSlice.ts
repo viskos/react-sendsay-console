7f6338c4364c1c43057d254b4142ceab784a3b0b
viskos <viskoos.fd@gmail.com>
2021-02-10 03:12:34 +0300

    fix: formated copy history item

--- src/store/slices/consoleSlice.ts
+++ src/store/slices/consoleSlice.ts
@@ -1,78 +1,88 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 import api from '../../helpers/sendsay';
 
-
 type TConsoleState = {
-    request: string | null,
-    response: string | null,
-    isError: boolean
-    requestsHistory: any
-}
+	request: string | null;
+	response: string | null;
+	isError: boolean;
+	requestsHistory: any;
+};
 
 const initialState: TConsoleState = {
-    request: null,
-    response: null,
-    isError: false,
-    requestsHistory: []
-}
+	request: null,
+	response: null,
+	isError: false,
+	requestsHistory: [],
+};
 
-export const asyncRequest = (payload: any) => {
-    return async (dispatch: any) => {
-        api.sendsay.request(payload)
-            .then((res: any) => {
-                dispatch(successRequest({res, payload}))
-            })
-            .catch((e: any) => {
-                dispatch(errorRequest({e, payload}))
-            }) 
-    }
-}
+export const asyncRequest = (payload: string) => {
+	return async (dispatch: any) => {
+		api.sendsay
+			.request(payload)
+			.then((res: Response) => {
+				dispatch(successRequest({ res, payload }));
+			})
+			.catch((e: Error) => {
+				dispatch(errorRequest({ e, payload }));
+			});
+	};
+};
 
 const consoleSlice = createSlice({
-    name: 'console',
-    initialState,
-    reducers: {
-        successRequest(state, action) {
-            state.response = action.payload.res
-            state.requestsHistory.unshift({
-                id: Date.now(),
-                success: true,
-                action: action.payload.payload.action
-            })
-            localStorage.setItem('history', JSON.stringify(state.requestsHistory))
-        },
-        errorRequest(state, action) {
-            state.response = action.payload.e
-            state.isError = true
-            state.requestsHistory.unshift({
-                id: Date.now(),
-                success: false,
-                action: action.payload.payload.action
-            })
-        },
-        clearHistory(state) {
-            state.requestsHistory = []
-            localStorage.setItem('history', JSON.stringify([]))
-        },
-        checkHistory(state) {
-            if (localStorage.getItem('history')) {
-                let history: any = localStorage.getItem('history')
-                state.requestsHistory = JSON.parse(history)
-            }
-        },
-        deleteHistoryItem(state, action) {
-            state.requestsHistory = state.requestsHistory.filter((e: any) => e.id !== action.payload)
-            localStorage.setItem('history', JSON.stringify(state.requestsHistory))
-        },
-        copyActionHistoryItem(state, action) {
-            let historyAction = state.requestsHistory.filter((e: any) => e.id === action.payload)
-            navigator.clipboard
-                .writeText(`{"action": "${historyAction[0].action}"}`)
-                .then(() => console.log('copy'))
-                .catch((e: any) => console.error('error copy', e));
-        }
-    }
-})
+	name: 'console',
+	initialState,
+	reducers: {
+		successRequest(state, action) {
+			state.response = action.payload.res;
+			state.requestsHistory.unshift({
+				id: Date.now(),
+				success: true,
+				action: action.payload.payload.action,
+			});
+			localStorage.setItem('history', JSON.stringify(state.requestsHistory));
+		},
+		errorRequest(state, action) {
+			state.response = action.payload.e;
+			state.isError = true;
+			state.requestsHistory.unshift({
+				id: Date.now(),
+				success: false,
+				action: action.payload.payload.action,
+			});
+			localStorage.setItem('history', JSON.stringify(state.requestsHistory));
+		},
+		clearHistory(state) {
+			state.requestsHistory = [];
+			localStorage.setItem('history', JSON.stringify([]));
+		},
+		checkHistory(state) {
+			if (localStorage.getItem('history')) {
+				let history: any = localStorage.getItem('history');
+				state.requestsHistory = JSON.parse(history);
+			}
+		},
+		deleteHistoryItem(state, action) {
+			state.requestsHistory = state.requestsHistory.filter((e: any) => e.id !== action.payload);
+			localStorage.setItem('history', JSON.stringify(state.requestsHistory));
+		},
+		copyActionHistoryItem(state, action) {
+			let historyAction = state.requestsHistory.filter((e: any) => e.id === action.payload);
+			navigator.clipboard
+				.writeText(
+					JSON.stringify(JSON.parse(`{"action": "${historyAction[0].action}"}`), undefined, 2)
+				)
+				.then(() => console.log('copy'))
+				.catch((e: any) => console.error('error copy', e));
+		},
+	},
+});
 
-export default consoleSlice.reducer
-export const { successRequest, errorRequest, clearHistory, checkHistory, deleteHistoryItem, copyActionHistoryItem } = consoleSlice.actions
+export default consoleSlice.reducer;
+export const {
+	successRequest,
+	errorRequest,
+	clearHistory,
+	checkHistory,
+	deleteHistoryItem,
+	copyActionHistoryItem,
+} = consoleSlice.actions;
